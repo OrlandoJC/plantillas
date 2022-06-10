d86e267d65a558ca9140c96de6db44b72001e16c
orlandoJc <cuximyamaorlandodejesus@gmail.com>
2022-06-10 18:38:55 -0500

    desafio completado - plantillas

---
+++ handlebars/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+const { engine } = require("express-handlebars")
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+app.set("views", "./views")
+app.use(express.static(path.join(__dirname, '/public/')))
+
+const data = {
+    products : [
+    {
+        id: 1,
+        name: "Nike Sportswear",
+        price: 1400,
+        pictureUrl: "https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/nike-dunk-256.png",
+    }
+]}
+
+app.get("/", (req, res) => {
+    res.render("index", null)
+})
+
+app.post("/productos", (req, res) => {
+    data.products.push({...req.body})
+    res.render("index", null)
+})
+
+app.get("/productos", (req, res) => {
+    res.render("productos", data)
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
